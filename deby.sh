82d97c0fe8ede3ada5b5377602af2fbf472ce030
رشيد قائد الوصابي <wusaby.rush@zohomail.com>
2022-03-19 11:55:35 +0000

    disable set flatpak until fix code for user not root

--- deby.sh
+++ deby.sh
@@ -137,5 +137,5 @@ if [ -z `command -v flatpak` ]; then
 	[ -z `command -v zenity` ] && echo $'flatpak is not installed \nto install it visit https://flatpak.org/setup/' || zenity --error --width=400 --text="flatpak is not installed \nto install it visit https://flatpak.org/setup/"
 	exit 1
 else
-	flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && zenity --info --text="Add flathub repo done." || zenity --error --text="An error has ocurred, check network."
+	#flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && zenity --info --text="Add flathub repo done." || zenity --error --text="An error has ocurred, check network."
 fi
