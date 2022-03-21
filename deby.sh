7e061a8500cde75bed88e17167aad489d19e9323
رشيد قائد الوصابي <wusaby.rush@zohomail.com>
2022-03-21 14:05:22 +0000

    configure podman search

--- deby.sh
+++ deby.sh
@@ -133,9 +133,20 @@ fi
 ## remove sources.list
 rm -f /etc/apt/sources.list /etc/apt/sources.list.save
 
+## configure flatpak
 if [ -z `command -v flatpak` ]; then
 	[ -z `command -v zenity` ] && echo $'flatpak is not installed \nto install it visit https://flatpak.org/setup/' || zenity --error --width=400 --text="flatpak is not installed \nto install it visit https://flatpak.org/setup/"
 	exit 1
 else
 	#flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && zenity --info --text="Add flathub repo done." || zenity --error --text="An error has ocurred, check network."
+fi
+
+
+
+## configure podman
+if [ -z `command -v podman` ]; then
+	[ -z `command -v zenity` ] && echo $'podman is not installed \nto install it visit https://podman.io' || zenity --error --width=400 --text="podman is not installed \nto install it visit https://podman.io"
+	exit 1
+else
+	echo 'unqualified-search-registries = ["docker.io", "quay.io"]' > /etc/containers/registries.conf.d/search.conf && zenity --info --text="Add podman search done." || zenity --error --text="An error has ocurred, check privilages."
 fi
