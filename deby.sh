6da3d165dd95b73abb896b682a7834262a96d60b
رشيد قائد الوصابي <wusaby.rush@zohomail.com>
2022-09-21 14:34:51 +0000

    Add limit user instructions

--- deby.sh
+++ deby.sh
@@ -142,3 +142,7 @@ if [ -z `command -v podman` ]; then
 else
 	echo 'unqualified-search-registries = ["docker.io", "quay.io"]' > /etc/containers/registries.conf.d/search.conf && zenity --info --text="Add podman search done." || zenity --error --text="An error has ocurred, check privilages."
 fi
+
+# limit users 
+# usermod -L --expiredate 2022-09-21 user
+# usermod -U --expiredate '' user
