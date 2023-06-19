27a7faee220896faa07cbf7e8dc18af180571c99
رشيد قائد الوصابي <wusaby.rush@zohomail.com>
2023-06-19 16:35:19 +0000

    Correct extra

--- extra.sh
+++ extra.sh
@@ -1,3 +1,11 @@
+## configure flatpak
+if [ -z `command -v flatpak` ]; then
+	[ -z `command -v zenity` ] && echo $'flatpak is not installed \nto install it visit https://flatpak.org/setup/' || zenity --error --width=400 --text="flatpak is not installed \nto install it visit https://flatpak.org/setup/"
+	exit 1
+else
+	#flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && zenity --info --text="Add flathub repo done." || zenity --error --text="An error has ocurred, check network."
+fi
+
 ## configure podman
 if [ -z `command -v podman` ]; then
 	[ -z `command -v zenity` ] && echo $'podman is not installed \nto install it visit https://podman.io' || zenity --error --width=400 --text="podman is not installed \nto install it visit https://podman.io"
